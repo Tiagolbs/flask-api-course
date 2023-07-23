2b3374b49f8c70118746fb17bf13cc3d9847c137
Tiago Seben <tiagolbs31@gmail.com>
2023-07-22 21:30:02 -0300

    Initial commit

---
+++ resources/__init__.py
@@ -0,0 +1,4 @@
+from resources.item import ItemModel, blp as ItemBlueprint
+from resources.store import StoreModel, blp as StoreBlueprint
+from resources.tag import TagModel, blp as TagBlueprint
+from resources.user import UserModel, blp as UserBlueprint
